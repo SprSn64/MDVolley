5bebb63dd49126f0ffb412ab6687a143dda18e18
SprSn64 <136403175+SprSn64@users.noreply.github.com>
2026-09-14 17:34:11 +1000

    player stuff

--- src/main.c
+++ src/main.c
@@ -10,6 +10,8 @@
 u32 timer = 0;
 u16 gameState = GAME_ACTIVE;
 
+PlayerChar players[4];
+
 u8 between(s16 val, s16 min, s16 max){
 	return val >= min && val <= max;
 }
@@ -39,11 +41,11 @@ void hInterrupt(){
 
 	//subScroll += 4 * HINT_COUNT;
 
-	if(currScanline >= 128)
-		VDP_setHorizontalScroll(BG_A, currScanline + 344);
+	if(currScanline >= 94)
+		VDP_setHorizontalScroll(BG_A, (currScanline >> 1) - 80);
 
-	if(between(currScanline, 104, 168))
-		VDP_setHorizontalScroll(BG_B, currScanline + 368);
+	if(between(currScanline, 72, 136))
+		VDP_setHorizontalScroll(BG_B, (currScanline >> 1) - 48);
 }
 
 void vInterrupt(){
@@ -91,12 +93,12 @@ int main(bool hardReset){
 	}
 
 	//40x28
-	VDP_loadTileData(netTiles, 64, 1, 0);
-	fillRect(BG_A, 3, (Vector2){0, 16}, (Vector2){40, 12});
-	fillRect(BG_A, 1, (Vector2){2, 18}, (Vector2){36, 8});
+	VDP_loadTileData(netTiles, 512, 1, 0);
+	fillRect(BG_A, 3, (Vector2){0, 13}, (Vector2){40, 14});
+	fillRect(BG_A, 1, (Vector2){2, 14}, (Vector2){36, 12});
 
-	fillRect(BG_B, TILE_ATTR_FULL(PAL0, 1, 0, 0, 64), (Vector2){20, 14}, (Vector2){1, 12});
-	fillRect(BG_B, TILE_ATTR_FULL(PAL0, 1, 0, 1, 64), (Vector2){21, 14}, (Vector2){1, 12});
+	fillRect(BG_B, TILE_ATTR_FULL(PAL0, 1, 0, 0, 512), (Vector2){19, 10}, (Vector2){1, 16});
+	fillRect(BG_B, TILE_ATTR_FULL(PAL0, 1, 0, 1, 512), (Vector2){20, 10}, (Vector2){1, 16});
 
 	SYS_disableInts();
 	{
@@ -107,17 +109,26 @@ int main(bool hardReset){
 	}
 	SYS_enableInts();
 
-	ballEntity = addEntity(160, 64, BHV_NONE, SPRITE_SIZE(3, 3), TILE_ATTR_FULL(PAL0, 0, 0, 0, ballTileLoc));
+	players[0] = (PlayerChar){NULL, NULL, CHARCONTROL_JOY1, CHAR_GIRL, (Vector2){80, 140}, (Vector2){0, 0}, (Vector2){0, 0}, (Vector2){-12, -32}, 0};
+	players[1] = (PlayerChar){NULL, NULL, CHARCONTROL_JOY2, CHAR_FREAK, (Vector2){80, 180}, (Vector2){0, 0}, (Vector2){0, 0}, (Vector2){-12, -24}, 0};
+	players[2] = (PlayerChar){NULL, NULL, CHARCONTROL_CPU, CHAR_GIRL | CHAR_TEAM, (Vector2){240, 140}, (Vector2){0, 0}, (Vector2){0, 0}, (Vector2){-12, -32}, 0};
+	players[3] = (PlayerChar){NULL, NULL, CHARCONTROL_NONE, CHAR_NONE, (Vector2){80, 180}, (Vector2){0, 0}, (Vector2){0, 0}, (Vector2){-12, -32}, 0};
 
-	playerEntity[0] = addEntity(160, 120, BHV_NONE, SPRITE_SIZE(3, 3), TILE_ATTR_FULL(PAL0, 0, 0, 0, playerTileLoc));
+	ballEntity = addEntity(200, 64, BHV_NONE, SPRITE_SIZE(3, 3), TILE_ATTR_FULL(PAL0, 0, 0, 0, ballTileLoc));
+
+	players[0].plrEntity = addEntity(120, 120, BHV_NONE, SPRITE_SIZE(3, 3), TILE_ATTR_FULL(PAL0, 0, 0, 0, playerTileLoc));
 	addEntity(0, 24, BHV_LINK, SPRITE_SIZE(3, 1), TILE_ATTR_FULL(PAL0, 0, 0, 0, playerTileLoc + 9));
 
-	playerEntity[1] = addEntity(160, 120, BHV_NONE, SPRITE_SIZE(3, 3), TILE_ATTR_FULL(PAL1, 0, 0, 0, freakTileLoc));
+	players[1].plrEntity = addEntity(200, 80, BHV_NONE, SPRITE_SIZE(3, 3), TILE_ATTR_FULL(PAL1, 0, 0, 0, freakTileLoc));
 	//addEntity(0, 24, BHV_LINK, SPRITE_SIZE(3, 1), TILE_ATTR_FULL(PAL1, 0, 0, 1, playerTileLoc + 9));
 
+	players[2].plrEntity = addEntity(200, 160, BHV_NONE, SPRITE_SIZE(3, 3), TILE_ATTR_FULL(PAL1, 0, 0, 0, playerTileLoc));
+	addEntity(0, 24, BHV_LINK, SPRITE_SIZE(3, 1), TILE_ATTR_FULL(PAL1, 0, 0, 0, playerTileLoc + 9));
+
 	shadowEntity = addEntity(160, 120, BHV_NONE, SPRITE_SIZE(3, 1), TILE_ATTR_FULL(PAL0, 0, 0, 0, ballShadowLoc));
-	playerShadow[0] = addEntity(160, 120, BHV_NONE, SPRITE_SIZE(3, 1), TILE_ATTR_FULL(PAL0, 0, 0, 0, ballShadowLoc));
-	playerShadow[1] = addEntity(160, 140, BHV_NONE, SPRITE_SIZE(3, 1), TILE_ATTR_FULL(PAL0, 0, 0, 0, ballShadowLoc));
+	players[0].shadowEntity = addEntity(160, 120, BHV_NONE, SPRITE_SIZE(3, 1), TILE_ATTR_FULL(PAL0, 0, 0, 0, ballShadowLoc));
+	players[1].shadowEntity = addEntity(160, 140, BHV_NONE, SPRITE_SIZE(3, 1), TILE_ATTR_FULL(PAL0, 0, 0, 0, ballShadowLoc));
+	players[2].shadowEntity = addEntity(160, 140, BHV_NONE, SPRITE_SIZE(3, 1), TILE_ATTR_FULL(PAL0, 0, 0, 0, ballShadowLoc));
 	
 	while(1){
 		timer++;
@@ -152,12 +163,7 @@ s16 ballY = 60;
 s16 ballSubPos[3] = {0, 0, 0};
 s16 ballVel[3] = {0, 0, 0};
 
-Vector2 playerPos = {160, 120};
-s16 playerSubPos[2] = {0, 0};
-s16 playerVel[2] = {0, 0};
-u8 hitTimer = 0;
-
-void updatePlayer(Entity* plr, u16 joy);
+void updatePlayer(PlayerChar* plr);
 void gameLoop(){
 	ballVel[1] -= 4;
 	ballSubPos[0] += ballVel[0]; ballSubPos[1] += ballVel[1]; ballSubPos[2] += ballVel[2]; 
@@ -179,8 +185,8 @@ void gameLoop(){
 		ballSubPos[0] = 0;
 		ballVel[0] = -ballVel[0];
 	}
-	if(!between(ballPos.y, 12, 228)){
-		ballPos.y = ballPos.y < 120 ? 13 : 227;
+	if(!between(ballPos.y, 104, 228)){
+		ballPos.y = ballPos.y < 180 ? 104 : 227;
 		ballSubPos[2] = 0;
 		ballVel[2] = -ballVel[2];
 	}
@@ -190,21 +196,16 @@ void gameLoop(){
 		ballY = 0;
 	}
 
-	if(between(ballPos.x - playerPos.x, -12, 12) && between(ballPos.y - playerPos.y, -12, 12) && ballY < 16){
-		hitTimer = 32;
-		ballVel[1] = 360; 
-		ballVel[0] += (ballPos.x - playerPos.x) * 16; ballVel[2] += (ballPos.y - playerPos.y) * 16;
-		ballY = 16;
-	}
-
 	ballEntity->pos = (Vector2){ballPos.x - 12, ballPos.y - ballY - 22};
 	shadowEntity->pos = (Vector2){ballPos.x - 12, ballPos.y - 4};
 
-	updatePlayer(playerEntity[0], JOY_1);
+	for(int i=0; i < 4; i++){
+		if(players[i].character > 0) updatePlayer(&players[i]);
+	}
 }
 
-void updatePlayer(Entity* plr, u16 joy){
-	/*playerVel[0] += ((inputDown[joy] & BUTTON_RIGHT) != 0) - ((inputDown[joy] & BUTTON_LEFT) != 0);
+/*void updatePlayer(Entity* plr, u16 joy){
+	playerVel[0] += ((inputDown[joy] & BUTTON_RIGHT) != 0) - ((inputDown[joy] & BUTTON_LEFT) != 0);
 	playerVel[1] += ((inputDown[joy] & BUTTON_DOWN) != 0) - ((inputDown[joy] & BUTTON_UP) != 0);
 
 	playerSubPos[0] += playerVel[0]; playerSubPos[1] += playerVel[1];
@@ -218,7 +219,7 @@ void updatePlayer(Entity* plr, u16 joy){
 	}
 	playerVel[0] += playerVel[0] != 0 ? negate(playerVel[0] > 0) : 0;
 	playerVel[1] += playerVel[1] != 0 ? negate(playerVel[1] > 0) : 0;
-	*/
+	
 
 	playerPos.x += ((inputDown[joy] & BUTTON_RIGHT) != 0) - ((inputDown[joy] & BUTTON_LEFT) != 0);
 	playerPos.y += ((inputDown[joy] & BUTTON_DOWN) != 0) - ((inputDown[joy] & BUTTON_UP) != 0);
@@ -235,5 +236,59 @@ void updatePlayer(Entity* plr, u16 joy){
 			VDP_loadTileData(girlJumpTiles, playerTileLoc, 15, 0);
 			plr->next->size = SPRITE_SIZE(3, 2);
 		}
+	}
+}*/
+
+void updatePlayer(PlayerChar* plr){
+	if(!plr->plrEntity) return;
+
+	if(between(plr->controller, CHARCONTROL_JOY1, CHARCONTROL_JOY4)){
+		u8 currJoy = plr->controller - 1;
+
+		plr->pos.x += ((inputDown[currJoy] & BUTTON_RIGHT) != 0) - ((inputDown[currJoy] & BUTTON_LEFT) != 0);
+		plr->pos.y += ((inputDown[currJoy] & BUTTON_DOWN) != 0) - ((inputDown[currJoy] & BUTTON_UP) != 0);
+	}
+
+	if(plr->controller == CHARCONTROL_CPU){
+		plr->pos = (Vector2){
+			(plr->character & CHAR_TEAM) ? max(ballPos.x, 172) : min(ballPos.x, 148), 
+			ballPos.y
+		};
+	}
+
+	if(plr->character == CHAR_GIRL){
+		plr->plrEntity->tile = TILE_ATTR_FULL(PAL0, 0, 0, 1, playerTileLoc);
+		plr->plrEntity->next->tile = TILE_ATTR_FULL(PAL0, 0, 0, 1, playerTileLoc + 9);
+	}
+
+	if(plr->pos.x > 148 && !(plr->character & CHAR_TEAM)){
+		plr->pos.x = 148;
+	}
+	if(plr->pos.x < 172 && (plr->character & CHAR_TEAM)){
+		plr->pos.x = 172;
+	}
+
+	if(!between(plr->pos.y, 104, 240)){
+		plr->pos.y = plr->pos.y < 180 ? 104 : 240;
+	}
+
+	plr->plrEntity->pos = (Vector2){
+		plr->pos.x + plr->sprOffset.x, 
+		//plr->pos.y + plr->sprOffset.y - 12 * (plr->hitTimer > 0)
+		plr->pos.y + plr->sprOffset.y + ((plr->hitTimer * (plr->hitTimer - 32)) >> 4)
+	};
+	if(plr->shadowEntity)
+		plr->shadowEntity->pos = (Vector2){plr->pos.x - 12, plr->pos.y - 4};
+
+	if(between(ballPos.x - plr->pos.x, -12, 12) && between(ballPos.y - plr->pos.y, -12, 12) && ballY < 16){
+		plr->hitTimer = 32;
+		ballVel[1] = 360; 
+		ballVel[0] = negate((plr->character & CHAR_TEAM) > 0) * 180 + (ballPos.x - plr->pos.x) * 4;
+		ballVel[2] += (ballPos.y - plr->pos.y) * 16;
+		ballY = 16;
+	}
+
+	if(plr->hitTimer > 0){
+		plr->hitTimer -= 1;
 	}
 }
--- src/structs.h
+++ src/structs.h
@@ -35,23 +35,33 @@ typedef struct Entity{
 } Entity;
 
 typedef enum CharController{
-	CHARCONTROL_NONE = 0x00;
+	CHARCONTROL_NONE = 0x00,
 
-	CHARCONTROL_JOY1 = 0x01;
-	CHARCONTROL_JOY2 = 0x02;
-	CHARCONTROL_JOY3 = 0x03;
-	CHARCONTROL_JOY4 = 0x04;
+	CHARCONTROL_JOY1 = 0x01,
+	CHARCONTROL_JOY2 = 0x02,
+	CHARCONTROL_JOY3 = 0x03,
+	CHARCONTROL_JOY4 = 0x04,
 
-	CHARCONTROL_CPU = 0xFF;
+	CHARCONTROL_CPU = 0xFF
 } CharController;
+
+typedef enum Character{
+	CHAR_NONE = 0x00,
+	CHAR_GIRL = 0x01,
+	CHAR_FREAK = 0x02,
+
+	CHAR_TEAM = 0x80,
+	CHAR_CHAR = 0x7F //127 possible characters
+} Character;
 
 typedef struct PlayerChar{
 	Entity *plrEntity;
 	Entity *shadowEntity;
 	u8 controller;
+	u8 character;
 
 	Vector2 pos, subPos, vel, sprOffset;
 	u8 hitTimer;
-}
+} PlayerChar;
 
 #endif
